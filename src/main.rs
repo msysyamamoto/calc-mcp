53d7668f924e19ae17bf8336dba8b434f342ecc3
Masayasu Yamamoto <491754+msysyamamoto@users.noreply.github.com>
2025-06-19 22:36:36 +0900

    sse

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ use schemars::JsonSchema;
 use serde::Deserialize;
 use std::collections::HashMap;
 
-use rmcp::transport::streamable_http_server::{
-    session::local::LocalSessionManager, StreamableHttpService,
-};
+use rmcp::transport::sse_server::{SseServer, SseServerConfig};
 
 use tracing_subscriber::{
     layer::SubscriberExt,
@@ -344,23 +342,34 @@ async fn main() -> anyhow::Result<()> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let service = StreamableHttpService::new(
-        || CalculatorService {},
-        LocalSessionManager::default().into(),
-        Default::default(),
-    );
+    let config = SseServerConfig {
+        bind: BIND_ADDRESS.parse()?,
+        sse_path: "/sse".to_string(),
+        post_path: "/message".to_string(),
+        ct: tokio_util::sync::CancellationToken::new(),
+        sse_keep_alive: None,
+    };
+    let (sse_server, router) = SseServer::new(config);
+    let listener = tokio::net::TcpListener::bind(sse_server.config.bind).await?;
+    let ct = sse_server.config.ct.child_token();
 
-    let router = axum::Router::new().nest_service("/mcp", service);
-    let tcp_listener = tokio::net::TcpListener::bind(BIND_ADDRESS).await?;
-    let _ = axum::serve(tcp_listener, router)
-        .with_graceful_shutdown(async { tokio::signal::ctrl_c().await.unwrap() })
-        .await;
+    let server = axum::serve(listener, router).with_graceful_shutdown(async move {
+        ct.cancelled().await;
+        tracing::info!("sse server cancelled");
+    });
+
+    tokio::spawn(async move {
+        if let Err(e) = server.await {
+            tracing::error!(error = %e, "sse server shutdown with error");
+        }
+    });
+
+    let ct = sse_server.with_service(|| CalculatorService);
+
+    tokio::signal::ctrl_c().await?;
+    ct.cancel();
 
     Ok(())
-
-    // let service = CalculatorService.serve(stdio()).await?;
-    // service.waiting().await?;
-    // Ok(())
 }
 
 #[cfg(test)]
